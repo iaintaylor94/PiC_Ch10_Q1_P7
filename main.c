0260cae5dd205896c2e1d8bbd8cb2d3653fc76fd
Iain Taylor <iain.taylor.94@gmail.com>
2023-08-19 14:12:43 +0000

    copied program

--- main.c
+++ main.c
@@ -1,6 +1,45 @@
+// Program to count the number of words in a string
+
 #include <stdio.h>
+# include <stdbool.h>
+
+// Declare Functions
+bool alphabetic (const char);
+int countWords (const char []);
 
 int main(void) {
-  printf("Hello World\n");
+
+  const char text1[] = "Well here goes.";
+  const char text2[] = "And here we go... again.";
+
+  printf ("%s - words = %d\n", text1, countWords (text1));
+  printf ("%s - words = %d\n", text2, countWords (text2));
+  
   return 0;
+}
+
+// Define Functions
+bool alphabetic (const char c) {
+  if ( (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+    return true;
+  else
+    return false;
+}
+
+int countWords (const char string[]) {
+  int i, wordCount = 0;
+  bool lookingForWord = true;
+
+  for (i = 0; string[i] != '\0'; i++) {
+    if (alphabetic(string[i])) {
+      if (lookingForWord) {
+        wordCount++;
+        lookingForWord = false;
+      }
+    }
+    else {
+      lookingForWord = true;
+    }
+  }
+  return wordCount;
 }
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Programming in C
+## Chapter 10
+### Question 1: part 7
+
+This problem is from chapter 10 of the textbook[^1]. This program is to copy Program 10.7[^2] from the textbook. This program counts the number of words in a string.
+
+You may wish to run the program and compare its output.
+
+
+[^1]: Programming in C (Third edition) By Stephen G. Kochan - Page 230 : Q1
+[^2]: Programming in C (Third edition) By Stephen G. Kochan - Page 211
